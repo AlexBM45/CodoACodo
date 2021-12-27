4e135f4cbf4621735312decedfaf95a04c682d27
AlejandraBM <alejandrabenitezmtz@gmail.com>
2021-12-27 16:31:35 -0600

    Inicio JS

---
+++ 4.JavaScript/EjemplosClase/script.js
@@ -0,0 +1,22 @@
+console.log('Hola mundo desde archivo externo');
+console.log(10000);
+
+/*
+TIPOS DE DATOS
+    caracteres - String
+    numeros
+        enteros: -, 0, +
+        flotantes: los decimales llevan punto
+    booleanos
+*/
+
+// Concatenar tipos de datos
+console.log('La edad del usuario es ' + 10);
+console.log(10 + 10);
+console.log('10' + '10');
+console.log('10' + 1);
+
+
+// ALERT
+alert('Hola mundo');
+alert(10 + 10);
