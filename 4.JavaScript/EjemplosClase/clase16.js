0ff449ddd4a3a24c7a2d262e739434948d9386f6
AlejandraBM <alejandrabenitezmtz@gmail.com>
2022-01-07 22:46:29 -0600

    Clase de JS

---
+++ 4.JavaScript/EjemplosClase/clase16.js
@@ -0,0 +1,60 @@
+console.log('Bienvenidos');
+
+alert('Hola, probando ALERT');
+
+/* TIPOS DE DATOS
+    numéricos
+        enteros
+        decimales
+    caracteres
+        textos
+        string
+    booleanos
+*/
+
+console.log('Hola spy una cadena');
+console.log(10);
+
+
+/* JS se aplica en
+    front
+    back (NodeJS)
+    móvil (React Native)
+    app de escritorio (Electron)
+*/
+
+console.log(true);
+console.log(false);
+
+// suma
+console.log(10+2+4);
+
+// Concatenación
+console.log('Hola, esto es una suma: ' + 10+5+5);
+console.log('10' + 10);
+
+// Parseo: convierte tipos de datos
+console.log(1 + true);
+// toma a true como un uno
+
+// VARIABLES
+// se declaran con la palabra reservada VAR
+var nombre = 'Alejandro';
+var apellido = 'Zapata';
+var edad = 10;
+var robot = false;
+
+console.log(nombre);
+console.log(apellido);
+console.log(robot);
+
+console.log(nombre + edad);
+
+/* 
+ALGORITMO
+Secuencia de pasos para resolver un problema
+*/
+
+// Manipulación del DOM
+document.write('Hola');
+document.write(robot);
